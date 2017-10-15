c1eb281e867e34917c5d752edc245ece728b198b
WisdomSky <i.wisdomsky@gmail.com>
2017-10-15 12:51:49 +0800

    create database migrations for users, roles and posts tables

---
+++ database/migrations/2017_10_14_044532_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('post_id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->enum('type', ['published', 'draft', 'deleted']);
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
